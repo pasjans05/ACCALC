6e8e43b2aead5bd9b6934caba43819abe0536d0f
Stanisław Kardaś <127191146+pasjans05@users.noreply.github.com>
2025-12-16 14:54:52 +0100

    0.1 First working version. Refactor racebox data processing and distance function
    - Removed the `readTime` function for parsing time values.
    - Renamed `distance` to `distanceGeoM` for clarity.
    - Introduced a `run` variable to manage program state.
    - Updated data reading logic using `stringstream` for efficiency.
    - Added a condition to check distance and output time difference.

--- ACCALC/ACCALC.cpp
+++ ACCALC/ACCALC.cpp
@@ -27,41 +27,11 @@ using namespace std;
  
 ifstream racebox("src/RaceBox25-10-2025_18-15.csv");
 
-float readTime(string entry, int* i)
-{
-	double digit, time = 0;
-	int decimal = 0;
-	for (; entry[*i] != ','; (*i)++)
-	{
-		if (entry[*i] == '.')
-		{
-			decimal = 1;
-		}
-		else
-		{
-			digit = entry[*i] - '0';
-			if (!decimal)
-			{
-				time *= 10;
-				time += digit;
-			}
-			else
-			{
-				for (int j = 0; j < decimal; j++)
-					digit /= 10;
-				time += digit;
-				decimal++;
-			}
-		}
-	}
-	return time;
-}
-
 double degreesToRadians(double degrees) {
 	return degrees * PI / 180;
 }
 
-double distance(double lat1, double lon1, double lat2, double lon2)
+double distanceGeoM(double lat1, double lon1, double lat2, double lon2)
 {
 	double earthRadiusKm = 6371.0;
 
@@ -85,32 +55,9 @@ int main()
 	string entry; // one line from racebox file
 	double time = 0, latitude = 0, longitude = 0, time0 = 0, lat0 = 0, long0 = 0;
 	int speed = 0;
-	bool isRead[COLS]; // variable to track whether we already read data from that column - columns numbered as in racebox csv header
-	// 0 Record; 1 Time; 2 Latitude; 3 Longitude; 4 Altitude; 5 Speed; 6 GForceX; 7 GForceY; 8 GForceZ; 9 Lap; 10 GyroX; 11 GyroY; 12 GyroZ;
+	bool run;
 	getline(racebox, entry);
-	/*while (getline(racebox, entry))
-	{
-		// cout << entry << endl;
-		for (int i = 0; i < COLS; i++)
-			isRead[i] = false;
-		// reading data from entry:
-		for (int i = 1; i < entry.length(); i++)
-		{
-			if (entry[i - 1] == ',' && !isRead[Time])
-			{
-				time = readTime(entry, &i);
-				isRead[Time] = true;
-			}
-			if (entry[i - 1] == ',' && !isRead[Latitude])
-			{
-				latitude = readTime(entry, &i);
-				isRead[Latitude] = true;
-			}
-			cout << time << '\t' << latitude << endl;
-		}
-	}*/
-
-
+	run = false;
 	while (getline(racebox, entry))
 	{
 		string field;
@@ -130,10 +77,16 @@ int main()
 			time0 = time;
 			lat0 = latitude;
 			long0 = longitude;
+			run = true;
 		}
+		if (run && (int)distanceGeoM(lat0, long0, latitude, longitude) == 75)
+		{
+			cout << time - time0 << endl;
+			run = false;
+		}
+			
 		// cout << fixed << setprecision(10) << time << '\t' << latitude << '\n';
 	}
-
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
